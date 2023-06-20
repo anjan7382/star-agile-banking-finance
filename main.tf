ab700f64983b05142bf5cbcd3fd8a39cf46b2360
anjan7382 <129925119+anjan7382@users.noreply.github.com>
2023-06-20 21:59:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,31 +1,16 @@
 provider "aws" {
-  region = "ap-south-1"
-}
-
-resource "aws_instance" "test_server" {
-  ami           = "ami-03a933af70fa97ad2"
-  instance_type = "t2.micro"
-  key_name      = "devopslab"
-  vpc_security_group_ids = ["sg-0a10474335ac41ac2"]
-
-  tags = {
-    Name = "test-server"
-  }
+  region = "eu-north-1"
 }
 
 resource "aws_instance" "production_server" {
-  ami           = "ami-03a933af70fa97ad2"  
-  instance_type = "t2.micro"               
-  key_name      = "devopslab"          
-  vpc_security_group_ids = ["sg-0a10474335ac41ac2"]  
+  ami           = "ami-0cf13cb849b11b451"  
+  instance_type = "t3.medium"               
+  key_name      = "devopslabs.pem"          
+  vpc_security_group_ids = ["vpc-047ede5642df03eb4"]  
 
   tags = {
     Name = "production-server"
   }
-}
-output "test_server_ip" {
-  value = aws_instance.test_server.public_ip
-}
 
 output "production_server_ip" {
   value = aws_instance.production_server.public_ip
